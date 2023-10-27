2b1e2affa4cd5b4c011f608914bb071849f9fd7d
ArthurYang <64062290+c-arthur-yang@users.noreply.github.com>
2023-10-27 14:51:28 +0800

    Add files via upload

--- utils/opt.py
+++ utils/opt.py
@@ -8,7 +8,7 @@ parser.add_argument('--gpu_id', type=str, default='0')
 parser.add_argument('--modelname', type=str, default="SGAE")
 parser.add_argument('--cuda', type=bool, default=True)
 parser.add_argument('--seed', type=int, default=1)
-parser.add_argument('--transport_rate', type=float, default=0.2)
+parser.add_argument('--dataset', type=str, default='merfish')
 
 # hyperparameters
 parser.add_argument('--n_clusters', type=int, default=20)
@@ -17,16 +17,15 @@ parser.add_argument('--k_nn', type=int, default=3)
 parser.add_argument('--alpha_value', type=float, default=0.2)
 
 # training parameters
-parser.add_argument('--train_who', type=int, default=[1, 1, 1, 1], nargs='+')  #
+parser.add_argument('--train_who', type=int, default=[1, 1, 1, 1], nargs='+')
 parser.add_argument('--n_epochs', type=int, default=1000, nargs='+')
-parser.add_argument('--patience', type=int, default=30) #
+parser.add_argument('--patience', type=int, default=30)
 
 parser.add_argument('--batch_size', type=int, default=256)
 parser.add_argument('--lr', type=float, default=1e-4)
 
 # loss weights
 parser.add_argument('--lambda_value', type=float, default=10)
-parser.add_argument('--gamma_value', type=float, default=1e3)
 
 # model parameters
 parser.add_argument('--n_inputs', type=int, default=100)
