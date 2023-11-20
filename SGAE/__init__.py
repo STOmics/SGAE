bbe8b421f432260f6f6bb3a282fde9648984da26
ArthurYang <64062290+c-arthur-yang@users.noreply.github.com>
2023-11-20 16:18:50 +0800

    Update __init__.py

--- SGAE/__init__.py
+++ SGAE/__init__.py
@@ -9,6 +9,6 @@
 __author__ = "Chao Yang"
 __email__ = "yangchao4@genomics.cn"
 
-from utils import config, gem2h5ad, gpu_memory_log, utils
-from models import AE, discriminator, GAE, Loss, Pre_model, readout, SGAE_model
-from trainers import train_HR
+from .utils import config, gem2h5ad, gpu_memory_log, utils
+from .models import AE, discriminator, GAE, Loss, Pre_model, readout, SGAE_model
+from .trainers import train_HR
